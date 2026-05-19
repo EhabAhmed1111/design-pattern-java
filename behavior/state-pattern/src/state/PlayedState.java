8597fa4b8a62a58dde43f6f13594308387ab2973
Ehab Ahmed <ehab.ahmed0202@gmail.com>
2026-05-19 18:24:49 +0300

    feature: adding state design pattern

---
+++ behavior/state-pattern/src/state/PlayedState.java
@@ -0,0 +1,34 @@
+package state;
+
+import context.Player;
+
+public class PlayedState extends State{
+
+    PlayedState(Player player) {
+        super(player);
+        player.setPlaying(true);
+    }
+
+    @Override
+    public String onLock() {
+        player.changeState(new LockedState(player));
+        player.setCurrentTrackAfterStop();
+        return "Stop playing";
+    }
+
+    @Override
+    public String onPlay() {
+        player.changeState(new ReadyState(player));
+        return "Paused...";
+    }
+
+    @Override
+    public String onNext() {
+        return player.nextTrack();
+    }
+
+    @Override
+    public String onPrevious() {
+        return player.previousTrack();
+    }
+}
---
+++ behavior/state-pattern/src/state/ReadyState.java
@@ -0,0 +1,33 @@
+package state;
+
+import context.Player;
+
+public class ReadyState extends State{
+
+    public ReadyState(Player player) {
+        super(player);
+    }
+
+    @Override
+    public String onLock() {
+        player.changeState(new LockedState(player));
+        return "Locked...";
+    }
+
+    @Override
+    public String onPlay() {
+        String action = player.startPlayback();
+        player.changeState(new PlayedState(player));
+        return action;
+    }
+
+    @Override
+    public String onNext() {
+        return "Locked...";
+    }
+
+    @Override
+    public String onPrevious() {
+        return "Locked...";
+    }
+}
